36ca688af79e406c0da73bd77ae24eb25138e665
Salvador Villalon Jr <salvadorvillalon54@gmail.com>
2024-09-23 12:56:41 -0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
+
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			{/* By adding Inter to the body element, the font will be applied
+			throughout your application. Here, you're also adding the Tailwind
+			antialiased class which smooths out the font. */}
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
